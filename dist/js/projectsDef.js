086052daa1612ae3fef9483ec1005ca6707f016b
Kesther04 <108943282+Kesther04@users.noreply.github.com>
2024-08-25 13:05:44 +0100

    single-page

---
+++ dist/js/projectsDef.js
@@ -0,0 +1,105 @@
+// elements
+let btns = document.querySelectorAll(".projects-selector .btn");
+let projectsContainer = document.querySelector(".projects-box");
+
+
+// to handle the btn project navigation
+btns.forEach(btn => btn.addEventListener('click',(e)=>{
+
+    btns.forEach(item => item.classList.remove('active'));
+    if (btn == e.target) {
+        btn.classList.add('active');
+    }
+    
+    if (e.target.value == "tech") {
+        //to fetch data from tech.json    
+        fetchedData('./dist/js/json/tech.json');
+    }
+    else if (e.target.value == "copy") {
+
+        // to fetch data from copy.json
+        fetchedData('./dist/js/json/copy.json');
+    }
+    
+}));
+
+// page onload handler
+window.onload = () => {
+    fetchedData('./dist/js/json/tech.json');
+}
+
+// Function for Fetching Data
+const fetchedData = (link) => {
+    fetch(link).then(response => response.json()).then(
+        (data) => {
+
+            projectsContainer.replaceChildren();
+            
+            // To Generate Random Values
+            let varr = [];
+            let carr = [];
+            for (let i = 0; i < data.projects.length; i++) {
+                varr.push(i);
+            }
+
+            for (let i = 1; i <= 5; i++) {
+                const randomIndex = Math.floor(Math.random()*varr.length)
+                carr.push(varr[randomIndex])
+                varr.splice(randomIndex,1);
+            }
+            
+            
+            // To map through fetched data  and display according to random generated values
+            data.projects.map((item,index) => {
+                carr.map((citm) => {
+                    if (citm == index) {
+                        
+                        let content = document.createElement('div');
+                        content.setAttribute('class','projects-content');
+    
+                        let contentImg = document.createElement('div');
+                        contentImg.setAttribute('class','projects-content-img');
+                        
+                        let img = document.createElement('img');
+                        let imgItem = [item.img1,item.img2];
+                        let imgRand = Math.floor(Math.random()*2);
+
+                        img.setAttribute('src',`./dist/img/${imgItem[imgRand]}`);
+                        img.setAttribute('alt',`${item.name}`);
+
+                        let contentDiv = document.createElement('div');
+                        contentDiv.setAttribute('class','projects-content-txt');
+                        
+                        let contentDivP = document.createElement('p');
+                        contentDivP.innerText = item.text;
+
+                        contentDiv.appendChild(contentDivP);  
+
+                        let linkHolder = document.createElement('div');
+                        
+
+                        item.link.map((link) => {
+                            let contentDivBtn = document.createElement('button');
+                            let contentDivA = document.createElement('a');
+                            contentDivA.setAttribute('href',link.hlink);
+                            contentDivA.innerHTML = link.desc;
+                            contentDivBtn.appendChild(contentDivA);
+                            linkHolder.appendChild(contentDivBtn);
+                        });
+
+                        contentDiv.appendChild(linkHolder);  
+                        contentImg.appendChild(img);
+                        contentImg.appendChild(contentDiv);
+                        content.appendChild(contentImg);
+                        projectsContainer.appendChild(content);
+                               
+                    }
+                
+                }); 
+                    
+            });           
+        }
+    );
+}
+    
+   
